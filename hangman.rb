f6876a5e25d4fd4ee11c3db9c5080a4b79568292
CassieV <obicassievirtue@gmail.com>
2022-12-16 12:58:08 +0100

    Create method to select words between 5 and 12

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,12 @@
-file = File.open("guess_words.txt", "r")
-contents = file.read
-words = contents.split
-secret_word = words.sample
-puts secret_word
+def choose_word
+  file = File.open("guess_words.txt", "r")
+  contents = file.read
+  words = contents.split
+  secret_word = words.sample
+  while secret_word.length < 5 || secret_word.length > 12
+    secret_word = words.sample
+  end
+  file.close
+  puts secret_word
+end
+choose_word
